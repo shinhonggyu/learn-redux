9a42d843f46b48df2abdaaa9235c45a3c772b188
shinhonggyu <zowoz0@naver.com>
2022-02-08 10:11:57 +0900

    create Redux modules & store

---
+++ src/modules/todos.js
@@ -0,0 +1,43 @@
+// 액션 타입 만들기
+const ADD_TODO = "counter/ADD_TODO";
+const TOGGLE_TODO = "counter/TOGGLE_TODO";
+
+// 액션 생성함수 만들기
+let nextId = 1;
+export const addTodo = (text) => ({
+  type: ADD_TODO,
+  todo: {
+    id: nextId++,
+    text,
+  },
+});
+
+export const toggleTodo = (id) => ({
+  type: TOGGLE_TODO,
+  id,
+});
+
+// 초기 상태 선언
+// 리듀서의 초기 상태는 꼭 객체타입일 필요 없습니다.
+// 배열이여도 되고, 원시 타입 (숫자, 문자열, 불리언 이여도 상관 없습니다.
+const initialState = [
+  {
+    id: 1,
+    text: "예시",
+    done: false,
+  },
+];
+
+// 리듀서 선언
+export default function todos(state = initialState, action) {
+  switch (action.type) {
+    case ADD_TODO:
+      return state.concat(action.todo);
+    case TOGGLE_TODO:
+      return state.map((todo) =>
+        todo.id === action.id ? { ...todo, done: !todo.done } : todo
+      );
+    default:
+      return state;
+  }
+}
